0e11b6fc7aec45b2a8ef4f4a28701d8bcb4c3841
Zeeshan <Zeeshan@Zeeshan-PC>
2017-01-01 11:05:24 +0530

    Need to upload class file

---
+++ GitFreaky/src/Test12.java
@@ -0,0 +1,13 @@
+
+public class Test12 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is test for Git Hub");
+		
+	}
+
+}
